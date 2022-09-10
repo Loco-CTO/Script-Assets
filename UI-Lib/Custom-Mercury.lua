a73cb070f610ec5aad524615f42b2214aecc2a9c
Loco-CTO <112562956+Loco-CTO@users.noreply.github.com>
2022-09-10 18:48:24 +0100

    Update Custom-Mercury.lua

--- UI-Lib/Custom-Mercury.lua
+++ UI-Lib/Custom-Mercury.lua
@@ -36,45 +36,13 @@ local Library = {
 			Secondary = Color3.fromHSV(240/360, 40/255, 63/255),
 			Tertiary = Color3.fromHSV(260/360, 60/255, 148/255),
 
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
-		Serika = {
-			Main = Color3.fromRGB(50, 52, 55),
-			Secondary = Color3.fromRGB(80, 82, 85),
-			Tertiary = Color3.fromRGB(226, 183, 20),
-
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
-		Dark = {
-			Main = Color3.fromRGB(30, 30, 35),
-			Secondary = Color3.fromRGB(50, 50, 55),
-			Tertiary = Color3.fromRGB(70, 130, 180),
-
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
-		Rust = {
-			Main = Color3.fromRGB(37, 35, 33),
-			Secondary = Color3.fromRGB(65, 63, 63),
-			Tertiary = Color3.fromRGB(237, 94, 38),
-
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
-		},
-		Aqua = {
-			Main = Color3.fromRGB(29, 28, 37),
-			Secondary = Color3.fromRGB(65, 63, 63),
-			Tertiary = Color3.fromRGB(51, 153, 137),
-
-			StrongText = Color3.fromHSV(0, 0, 1),        
+			StrongText = Color3.fromHSV(0, 0.0, 1),		
 			WeakText = Color3.fromHSV(0, 0, 172/255)
 		},
 		Custom = {
-			Main = Color3.fromRGB(29, 18, 39),
-			Secondary = Color3.fromRGB(54, 35, 71),
-			Tertiary = Color3.fromRGB(85, 55, 112),
+			Main = Color3.fromRGB(6, 4, 8),
+			Secondary = Color3.fromRGB(32, 20, 43),
+			Tertiary = Color3.fromRGB(28, 17, 37),
 
 			StrongText = Color3.fromHSV(0, 0, 1),        
 			WeakText = Color3.fromHSV(0, 0, 172/255)
